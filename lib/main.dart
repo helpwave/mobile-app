8663673e884b154087c731769cf63ae2145d73d7
Proffi <prof.pixel77@gmail.com>
2022-10-04 21:42:18 +0200

    feat: add homepage and make it accessible from landing

--- lib/main.dart
+++ lib/main.dart
@@ -18,19 +18,36 @@ class MyApp extends StatelessWidget {
         appBarTheme: const AppBarTheme(
           backgroundColor: Color.fromARGB(255, 68, 68, 255),
         ),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(
+                const Color.fromARGB(255, 68, 68, 255)),
+            padding:
+                MaterialStateProperty.all<EdgeInsets>(const EdgeInsets.all(18)),
+            side: MaterialStateProperty.all<BorderSide>(const BorderSide(
+              color: Color.fromARGB(255, 255, 255, 255),
+              width: 2,
+            )),
+            fixedSize: MaterialStateProperty.all<Size>(const Size(250, 50)),
+          ),
+        ),
         colorScheme: const ColorScheme(
           brightness: Brightness.dark,
-          primary: Color.fromARGB(255, 17, 17, 51),
-          onPrimary: Color.fromARGB(255, 255, 255, 255),
-          background: Color.fromARGB(255, 17, 17, 51),
+          primary: Color.fromARGB(255, 255, 255, 255),
+          onPrimary: Color.fromARGB(255, 0, 0, 0),
+          background: Color.fromARGB(255, 153, 153, 153),
           onBackground: Color.fromARGB(255, 255, 255, 255),
           secondary: Color.fromARGB(255, 68, 68, 255),
           onSecondary: Color.fromARGB(255, 255, 255, 255),
+          tertiary: Color.fromARGB(255, 153, 153, 153),
+          onTertiary: Color.fromARGB(255, 255, 255, 255),
           error: Color.fromARGB(255, 255, 51, 51),
           onError: Color.fromARGB(255, 255, 255, 255),
           surface: Color.fromARGB(255, 85, 85, 85),
           onSurface: Color.fromARGB(255, 255, 255, 255),
-          primaryContainer: Color.fromARGB(255, 17, 17, 51),
+          primaryContainer: Color.fromARGB(255, 153, 153, 153),
+          onPrimaryContainer: Color.fromARGB(255, 255, 255, 255),
+          outline: Color.fromARGB(255, 255, 255, 255),
         ),
       ),
       home: const LandingPage(),
---
+++ lib/pages/home.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size mediaQuery = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "helpwave",
+          textAlign: TextAlign.center,
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(20, 20, 20, mediaQuery.height * 0.07),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, mediaQuery.height * 0.2, 0,
+                        mediaQuery.height * 0.05),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        // TODO Add QuestionPage Route
+                        //Navigator.push(context, MaterialPageRoute(builder: (context) => SomePage(),))
+                      },
+                      child: const Text("Fragebogen"),
+                    ),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+                      // TODO Add Map Route
+                      //Navigator.push(context, MaterialPageRoute(builder: (context) => SomePage(),))
+                    },
+                    child: const Text("Notaufnahmekarte"),
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: OutlinedButton(
+                onPressed: () {
+                  // TODO Add Settings Route
+                  //Navigator.push(context, MaterialPageRoute(builder: (context) => SomePage(),))
+                },
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Theme.of(context).colorScheme.background)),
+                child: const Text("Einstellungen"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
