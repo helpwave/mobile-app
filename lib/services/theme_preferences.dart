96bdd371c84e9e5ea12122b3fd128f7e7a89c3bb
DasProffi <prof.pixel77@gmail.com>
2023-01-10 00:00:14 +0100

    refactor: refactor style and add documentation

--- lib/services/theme_preferences.dart
+++ lib/services/theme_preferences.dart
@@ -1,6 +1,8 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// Service for reading and writing the Theme Preference
 class ThemePreferences {
+  /// Key of the Shared Preference
   final String sharedPreferencesThemeKey = "is_dark_theme";
 
   setTheme(bool value) async {
