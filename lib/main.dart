aa32b4847944e1b666ec617578d0a5160125bd8a
DasProffi <felix.thape@gmail.com>
2023-01-21 01:38:03 +0100

    feat: change persistence to sql

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:helpwave/services/database_handler.dart';
 import 'package:helpwave/config/language.dart';
 import 'package:helpwave/pages/home_page.dart';
 import 'package:helpwave/services/introduction_model.dart';
@@ -11,14 +13,13 @@ import 'package:helpwave/services/language_model.dart';
 import 'package:helpwave/services/theme_model.dart';
 import 'package:helpwave/styling/dark_theme.dart';
 import 'package:helpwave/styling/light_theme.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   // initialize
+  DatabaseHandler().initDatabase();
   runApp(const MyApp());
   FlutterNativeSplash.remove();
 }
@@ -42,12 +43,11 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: Consumer3<ThemeModel, LanguageModel, IntroductionModel>(
-        builder: (_, ThemeModel themeNotifier, LanguageModel languageNotifier,
-            IntroductionModel introductionModel, __) {
+        builder:
+            (_, ThemeModel themeNotifier, LanguageModel languageNotifier, IntroductionModel introductionModel, __) {
           ThemeMode themeMode = ThemeMode.system;
           if (themeNotifier.isDark != null) {
-            themeMode =
-                themeNotifier.isDark! ? ThemeMode.dark : ThemeMode.light;
+            themeMode = themeNotifier.isDark! ? ThemeMode.dark : ThemeMode.light;
           }
           return MaterialApp(
             title: 'helpwave',
@@ -60,13 +60,8 @@ class MyApp extends StatelessWidget {
               GlobalWidgetsLocalizations.delegate,
               GlobalCupertinoLocalizations.delegate,
             ],
-            supportedLocales: languages
-                .map((language) =>
-                    Locale(language["Language"]!, language["Local"]!))
-                .toList(),
-            home: introductionModel.hasSeenIntroduction
-                ? const HomePage()
-                : const LandingPage(),
+            supportedLocales: languages.map((language) => Locale(language["Language"]!, language["Local"]!)).toList(),
+            home: introductionModel.hasSeenIntroduction ? const HomePage() : const LandingPage(),
             locale: Locale(languageNotifier.language),
           );
         },
--- lib/services/patient_persistence.dart
+++ lib/services/patient_persistence.dart
@@ -1,6 +1,5 @@
-import 'dart:convert';
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:helpwave/services/database_handler.dart';
 import 'package:helpwave/data_classes/patient_data.dart';
 
 /// The PersistenceService for [PatientData]
@@ -8,30 +7,73 @@ import 'package:helpwave/data_classes/patient_data.dart';
 /// Loads and Saves PatientData to a file on the users device
 class PatientPersistenceService {
   /// Singleton static variable
-  static final PatientPersistenceService _serviceSingleton =
-      PatientPersistenceService._internal();
+  static final PatientPersistenceService _serviceSingleton = PatientPersistenceService._internal();
 
-  /// Name of the file which will be stored on the device
-  final String _filename = "patientData.json";
+  /// The Id which will be the default patient id
+  static const int patientId = 0;
 
   factory PatientPersistenceService() => _serviceSingleton;
 
   PatientPersistenceService._internal();
 
-  Future<PatientData> load() async {
-    final directory = await getApplicationDocumentsDirectory();
-    File file = File('${directory.path}/$_filename');
+  Future<PatientData> load({int patientId = patientId}) async {
     try {
-      final contents = await file.readAsString();
-      return PatientData.fromJson(jsonDecode(contents));
+      // load data
+      Database db = await DatabaseHandler().database;
+      List<Map<String, Object?>> res = await db.query("Patient", where: "id  = ?", whereArgs: [patientId]);
+      if (res.isEmpty) {
+        return PatientData();
+      }
+      Map<String, Object?> patientMap = Map.of(res.first);
+      List<Map<String, Object?>> allergies = await db.query("Allergy", where: "patientId  = ?", whereArgs: [patientId]);
+      List<Map<String, Object?>> medications =
+          await db.query("Medication", where: "patientId  = ?", whereArgs: [patientId]);
+
+      // change data to fit map-format
+      patientMap.putIfAbsent("allergies",
+          () => Map<String, Object?>.fromEntries(allergies.map((e) => MapEntry(e["name"] as String, e["severity"]))));
+      patientMap.putIfAbsent("medication",
+          () => Map<String, Object?>.fromEntries(medications.map((e) => MapEntry(e["name"] as String, e["dosage"]))));
+      patientMap.remove("id");
+      if (patientMap["isOrganDonor"] != null) {
+        patientMap.update(
+          "isOrganDonor",
+          (value) => patientMap["isOrganDonor"] == 1,
+          ifAbsent: () => patientMap["isOrganDonor"] == 1,
+        );
+      }
+
+      return PatientData.fromJson(patientMap);
     } catch (e) {
       return PatientData();
     }
   }
 
   Future<void> save(PatientData patientData) async {
-    final directory = await getApplicationDocumentsDirectory();
-    File file = File('${directory.path}/$_filename');
-    file.writeAsString(patientData.serialize());
+    try {
+      // prepare data
+      Database db = await DatabaseHandler().database;
+      Map<String, dynamic> patientMap = patientData.toJson();
+      patientMap.putIfAbsent("id", () => patientId);
+      Map<String, dynamic> medication = patientMap.remove("medication");
+      Map<String, dynamic> allergies = patientMap.remove("allergies");
+
+      // insert data into db
+      await db.insert("Patient", patientMap, conflictAlgorithm: ConflictAlgorithm.replace);
+      medication.forEach((key, value) async {
+        await db.insert(
+          "Medication",
+          {"patientId": patientId, "name": key, "dosage": value},
+          conflictAlgorithm: ConflictAlgorithm.replace,
+        );
+      });
+      allergies.forEach((key, value) async {
+        await db.insert(
+          "Allergy",
+          {"patientId": patientId, "name": key, "severity": value},
+          conflictAlgorithm: ConflictAlgorithm.replace,
+        );
+      });
+    } catch (_) {}
   }
 }
