5bc681734dcc9f850b6e0706f73a119386a75f47
DasProffi <felix.thape@gmail.com>
2023-01-26 01:23:09 +0100

    feat: add service package

--- apps/helpwave/lib/pages/landing_page.dart
+++ apps/helpwave/lib/pages/landing_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:helpwave_theme/constants.dart';
+import 'package:helpwave_service/introduction.dart';
 import 'package:helpwave/pages/home_page.dart';
-import 'package:helpwave/services/introduction_model.dart';
 
 /// Landing-Page, if the user start the app for the first time
 class LandingPage extends StatelessWidget {
