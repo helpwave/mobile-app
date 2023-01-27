91f58123411892f9e2a3b5d9ca6efc8b398ae93c
Felix <67233923+DasProffi@users.noreply.github.com>
2023-01-27 01:25:06 +0100

    feat: add explicit typing on LocalizationExtension

--- packages/helpwave_localization/lib/src/localization_impl.dart
+++ packages/helpwave_localization/lib/src/localization_impl.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:helpwave_localization/l10n/app_localizations.dart';
 
 extension LocalizationExtension on BuildContext {
-  get localization => AppLocalizations.of(this);
+  AppLocalizations? get localization => AppLocalizations.of(this);
 }
