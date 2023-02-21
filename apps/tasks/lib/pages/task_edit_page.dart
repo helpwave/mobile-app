32c70ded146942cfa49333a1406ce019e1de4675
DasProffi <felix.thape@gmail.com>
2023-02-21 23:44:52 +0100

    feat: add task create page

--- apps/tasks/lib/pages/task_edit_page.dart
+++ apps/tasks/lib/pages/task_edit_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:helpwave_localization/localization.dart';
 import 'package:helpwave_theme/constants.dart';
+import 'package:helpwave_widget/loading.dart';
 
 /// Page for Editing or Creating a [Task]
 ///
@@ -32,10 +33,14 @@ class TaskEditPage extends StatefulWidget {
 }
 
 class _TaskEditPageState extends State<TaskEditPage> {
-  Future<Map<String, dynamic>> getTask() async {
+  Map<String, dynamic>? task;
+  bool hasError = false;
+
+  getTask() async {
     // TODO make ApiCall here with widget.taskID
+    // set hasError to true in case of an error
     await Future.delayed(const Duration(milliseconds: 500));
-    return {
+    task = {
       "id": "UUID",
       "name": "Task Name",
       "description": "Description",
@@ -59,7 +64,7 @@ class _TaskEditPageState extends State<TaskEditPage> {
       },
       "room": {
         "id": "UUID",
-        "name": "string",
+        "name": "Blue Room",
       },
       "ward": {
         "id": "UUID",
@@ -71,122 +76,168 @@ class _TaskEditPageState extends State<TaskEditPage> {
         "shortName": "string",
       },
     };
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getTask();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     const double padding = distanceDefault;
-    Size buttonSize =
-        Size(MediaQuery.of(context).size.width * 0.5 - padding, 50);
+    Size buttonSize = Size(MediaQuery.of(context).size.width - 2 * padding, 50);
 
-    return FutureBuilder(
-        future: getTask(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return LoadErrorWidget(
-              iconColor: Theme.of(context).colorScheme.primary,
-              errorText: context.localization!.errorOnLoad,
-            );
-          }
-          if (!snapshot.hasData) {
-            return LoadingSpinner(
-              text: context.localization!.loading,
-              color: Theme.of(context).colorScheme.primary,
-            );
-          }
+    return Builder(builder: (context) {
+      if (hasError) {
+        return Scaffold(
+          body: LoadErrorWidget(
+            iconColor: Theme.of(context).colorScheme.primary,
+            errorText: context.localization!.errorOnLoad,
+          ),
+        );
+      }
+      if (task == null) {
+        return Scaffold(
+          body: LoadingSpinner(
+            text: context.localization!.loading,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        );
+      }
 
-          Map<String, dynamic> task = snapshot.data[0];
-          List<Map<String, dynamic>> subtasks = task["subtask"];
-          List<Widget> subtaskWidgets = subtasks
-              .map((subtask) => Row(
-                    children: [
-                      Checkbox(
-                        value: subtask["done"],
-                        onChanged: (value) => setState(() => subtask["done"]),
-                      ),
-                      const SizedBox(width: distanceDefault),
-                      Flexible(
-                        child: TextFormField(
-                          onChanged: (value) => subtask["name"] = value,
-                          initialValue: subtask["name"],
-                        ),
-                      ),
-                    ],
-                  ))
-              .toList();
-          return Scaffold(
-            appBar: AppBar(title: Text(task["name"])),
-            floatingActionButtonLocation:
-                FloatingActionButtonLocation.centerFloat,
-            floatingActionButton: TextButton(
-              style: ButtonStyle(
-                shape: const MaterialStatePropertyAll(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(borderRadiusBig),
-                      bottomLeft: Radius.circular(borderRadiusBig),
-                    ),
-                  ),
-                ),
-                minimumSize: MaterialStatePropertyAll(buttonSize),
-              ),
-              onPressed: () => setState(() {
-                for (var element in subtasks) {
-                  element["done"] = true;
-                }
-                // TODO also change status of entire task to done
-                // TODO make api call here for update
-              }),
-              child: Text(context.localization!.finishAllTasks),
-            ),
-            body: SingleChildScrollView(
-              padding: const EdgeInsets.all(padding),
-              child: Column(
+      List<Map<String, dynamic>> subtasks = task!["subtasks"];
+      List<Widget> subtaskWidgets = subtasks
+          .map(
+            (subtask) => Padding(
+              padding: const EdgeInsets.symmetric(vertical: distanceSmall),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Card(
-                    margin: EdgeInsets.zero,
-                    shape: const RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.all(Radius.circular(paddingSmall))),
-                    child: Padding(
-                      padding: const EdgeInsets.all(paddingSmall),
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text("${context.localization!.room}:"),
-                              Text(task["room"]["name"]),
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text("${context.localization!.bed}:"),
-                              Text(task["bed"]["name"]),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
+                  Checkbox(
+                    visualDensity: VisualDensity.compact,
+                    value: subtask["done"],
+                    onChanged: (value) => setState(() {
+                      subtask["done"] = value;
+                    }),
                   ),
-                  ...subtaskWidgets,
-                  Center(
-                    child: TextButton(
-                      style: const ButtonStyle(
-                        backgroundColor:
-                            MaterialStatePropertyAll(Colors.transparent),
-                      ),
-                      onPressed: () => setState(() {
-                        subtasks.add({"done": false, "name": ""});
-                      }),
-                      child: Text(context.localization!.addSubtask),
+                  const SizedBox(width: distanceSmall),
+                  Flexible(
+                    child: TextFormField(
+                      decoration: const InputDecoration(contentPadding: EdgeInsets.all(distanceSmall)),
+                      maxLines: null,
+                      onChanged: (value) => subtask["name"] = value,
+                      initialValue: subtask["name"],
                     ),
                   ),
                 ],
               ),
             ),
-          );
-        });
+          )
+          .toList();
+      return Scaffold(
+        appBar: AppBar(title: Text(task!["name"])),
+        body: Column(
+          children: [
+            Card(
+              margin: const EdgeInsets.symmetric(horizontal: padding, vertical: 0),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(paddingSmall)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(paddingSmall),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("${context.localization!.room}:"),
+                        Text(task!["room"]["name"]),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: distanceSmall,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("${context.localization!.bed}:"),
+                        Text(task!["bed"]["name"]),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Flexible(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.all(padding),
+                child: Column(
+                  children: [
+                    ...subtaskWidgets,
+                    Center(
+                      child: TextButton.icon(
+                        icon: Icon(
+                          Icons.add,
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                        style: ButtonStyle(
+                          foregroundColor: MaterialStatePropertyAll(Theme.of(context).colorScheme.onBackground),
+                          backgroundColor: const MaterialStatePropertyAll(Colors.transparent),
+                          shape: const MaterialStatePropertyAll(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(paddingSmall),
+                              ),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {
+                          // Type cast need otherwise type error on line below assignment
+                          // ignore: unnecessary_cast
+                          Map<String, dynamic> subtask = {
+                            "name": "",
+                            "done": false,
+                          } as Map<String, dynamic>;
+                          task!["subtasks"].add(subtask);
+                          setState(() {});
+                        },
+                        label: Text(context.localization!.addSubtask),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Visibility(
+              visible: MediaQuery.of(context).viewInsets.bottom == 0,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: padding),
+                child: TextButton(
+                  style: ButtonStyle(
+                    shape: const MaterialStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(borderRadiusBig)),
+                      ),
+                    ),
+                    minimumSize: MaterialStatePropertyAll(buttonSize),
+                  ),
+                  onPressed: () => setState(() {
+                    for (var element in subtasks) {
+                      element["done"] = true;
+                    }
+                    // TODO also change status of entire task to done
+                    // TODO make api call here for update
+                  }),
+                  child: Text(context.localization!.finishAllTasks),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
