d9ab50c16ef3fac2bd73d7166cd22a90da826b51
Florian Sabonchi <sabonchi@posteo.de>
2023-02-22 21:25:23 +0100

    feat: add forgot password button

--- apps/tasks/lib/pages/login_page.dart
+++ apps/tasks/lib/pages/login_page.dart
@@ -20,74 +20,94 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     const padding = distanceDefault;
     double rowWidth = MediaQuery.of(context).size.width - 2 * padding;
+
     ButtonStyle loginButtonStyle = ButtonStyle(
-      side: MaterialStateProperty.all(const BorderSide(style: BorderStyle.solid)),
+      side: MaterialStateProperty.all(const BorderSide(style: BorderStyle.solid,
+          color: Colors.grey)),
       backgroundColor: MaterialStateProperty.all(Colors.transparent),
       minimumSize: MaterialStatePropertyAll(Size(rowWidth, 50)),
     );
 
     return Scaffold(
-      appBar: AppBar(title: Text(context.localization!.login)),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: padding),
-        child: TextButton(
-          style: loginButtonStyle,
-          onPressed: () {
-            if (_formKey.currentState!.validate()) {
-              _formKey.currentState!.save();
-              // TODO do use email password here
+        appBar: AppBar(title: Text(context.localization!.login)),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(bottom: padding),
+          child: TextButton(
+            style: loginButtonStyle,
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                _formKey.currentState!.save();
+                // TODO do use email password here
 
-              // TODO onSuccess push to app home-screen
-            }
-          },
-          child: Text(
-            context.localization!.login,
-            style: Theme.of(context).textTheme.titleMedium,
+                // TODO onSuccess push to app home-screen
+              }
+            },
+            child: Text(
+              context.localization!.login,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
           ),
         ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      body: Center(
-        child: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              padding: const EdgeInsets.all(padding),
-              child: Column(
-                children: [
-                  const SizedBox(height: distanceSmall),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        hintText: context.localization!.email
-                    ),
-                    onSaved: (value) => _email = value!,
-                    validator: (email) {
-                      if (email == null || email.isEmpty) {
-                        return context.localization!.emailNotValid;
-                      }
-                      if (!RegExp(r"^[a-zA-Z0-9a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email)) {
-                        return context.localization!.emailNotValid;
-                      }
-                      return null;
-                      },
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        body: Center(
+            child: Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(padding),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: distanceSmall),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            hintText: context.localization!.email),
+                        onSaved: (value) => _email = value!,
+                        validator: (email) {
+                          if (email == null || email.isEmpty) {
+                            return context.localization!.emailNotValid;
+                          }
+                          if (!RegExp(
+                                  r"^[a-zA-Z0-9a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(email)) {
+                            return context.localization!.emailNotValid;
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: distanceDefault),
+                      const SizedBox(height: distanceSmall),
+                      PasswordTextEditingField(
+                        hintText: context.localization!.password,
+                        onChanged: (value) => _password,
+                        onSaved: (value) => _password = value!,
+                        validator: (password) {
+                          if (password == null || password.isEmpty) {
+                            return context.localization!.noPassword;
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: distanceSmall),
+                      Row(
+                        children: [
+                          TextButton(
+                              style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                      Colors.transparent)),
+                              onPressed: () => {},
+                              child: Text(
+                                context.localization!.forgotPassword,
+                                style: const TextStyle(
+                                    decoration: TextDecoration.underline,
+                                    color: Colors.grey),
+                              )
+                          )
+                        ],
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: distanceDefault),
-                  const SizedBox(height: distanceSmall),
-                  PasswordTextEditingField(
-                    hintText: context.localization!.password,
-                    onChanged: (value) => _password,
-                    onSaved: (value) => _password = value!,
-                    validator: (password) {
-                      if (password == null || password.isEmpty) {
-                        return context.localization!.noPassword;
-                      }
-                      return null;
-                      },
-                  ),
-                ],
-              ),
+                )
             )
-        ),
-      )
+        )
     );
   }
 }
