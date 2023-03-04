a505d8b40430bbd993729bb5eade1b8c1ae13212
Florian Sabonchi <sabonchi@posteo.de>
2023-03-04 17:02:46 +0100

    feat: add divider for ListTile

--- apps/tasks/lib/pages/settings_page.dart
+++ apps/tasks/lib/pages/settings_page.dart
@@ -23,41 +23,43 @@ class _SettingsPageState extends State<SettingsPage> {
         children: [
           Padding(padding: const EdgeInsets.symmetric(horizontal: paddingSmall), child:
             Column(
-              children: [
-                ListTile(
-                  leading: const Icon(Icons.brightness_medium),
-                  title: Text(context.localization!.darkMode),
-                  trailing: Switch(value: isDarkTheme, onChanged: (bool value){
-                    setState(() {
-                      isDarkTheme = !isDarkTheme;
-                    });
-                  },),
-                ),
-                ListTile(
-                  leading: const Icon(Icons.language),
-                  title: Text(context.localization!.language),
-                  trailing: const Icon(Icons.arrow_forward),
-                  onTap: () => {
+              children:
+              ListTile.divideTiles(context: context,
+                  tiles: [
+                    ListTile(
+                    leading: const Icon(Icons.brightness_medium),
+                    title: Text(context.localization!.darkMode),
+                    trailing: Switch(value: isDarkTheme, onChanged: (bool value){
+                      setState(() {
+                        isDarkTheme = !isDarkTheme;
+                      });
+                    },),
+                  ),
+                    ListTile(
+                    leading: const Icon(Icons.language),
+                    title: Text(context.localization!.language),
+                    trailing: const Icon(Icons.arrow_forward),
+                    onTap: () => {
 
-                  },
-                ),
-                ListTile(
-                  leading: const Icon(Icons.info_outline),
-                  title: Text(context.localization!.licenses),
-                  trailing: const Icon(Icons.arrow_forward),
-                  onTap: () => {
+                    },
+                  ),
+                    ListTile(
+                    leading: const Icon(Icons.info_outline),
+                    title: Text(context.localization!.licenses),
+                    trailing: const Icon(Icons.arrow_forward),
+                    onTap: () => {
 
-                  },
-                ),
-                ListTile(
-                  leading: const Icon(Icons.logout),
-                  title: Text(context.localization!.logout),
-                  onTap: () => {
+                    },
+                  ),
+                    ListTile(
+                      leading: const Icon(Icons.logout),
+                      title: Text(context.localization!.logout),
+                      onTap: () => {
 
-                  },
-                )
-              ],
-            ),
+                      },
+                    )
+                  ]
+              ).toList())
           )
         ],
       ),
