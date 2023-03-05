c1231e8ec3cdcf4bdeaae8c29b46b2a9a5a693fe
Florian Sabonchi <sabonchi@posteo.de>
2023-03-05 18:24:01 +0100

    feat: replace hardcoded dark theme with MultiProvider

--- apps/tasks/lib/pages/settings_page.dart
+++ apps/tasks/lib/pages/settings_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:helpwave_localization/localization.dart';
 import 'package:helpwave_theme/constants.dart';
+import 'package:helpwave_theme/theme.dart';
+import 'package:provider/provider.dart';
 
 
 /// Screen for settings and other app options
@@ -12,58 +14,77 @@ class SettingsPage extends StatefulWidget{
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  bool isDarkTheme = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(context.localization!.settings),
-      ),
-      body: ListView(
-        children: [
-          Padding(padding: const EdgeInsets.symmetric(horizontal: paddingSmall), child:
-            Column(
-              children:
-              ListTile.divideTiles(context: context,
-                  tiles: [
-                    ListTile(
-                    leading: const Icon(Icons.brightness_medium),
-                    title: Text(context.localization!.darkMode),
-                    trailing: Switch(value: isDarkTheme, onChanged: (bool value){
-                      setState(() {
-                        isDarkTheme = !isDarkTheme;
-                      });
-                    },),
-                  ),
-                    ListTile(
-                    leading: const Icon(Icons.language),
-                    title: Text(context.localization!.language),
-                    trailing: const Icon(Icons.arrow_forward),
-                    onTap: () => {
 
-                    },
-                  ),
-                    ListTile(
-                    leading: const Icon(Icons.info_outline),
-                    title: Text(context.localization!.licenses),
-                    trailing: const Icon(Icons.arrow_forward),
-                    onTap: () => {
+    bool isDarkTheme = false;
 
-                    },
-                  ),
-                    ListTile(
-                      leading: const Icon(Icons.logout),
-                      title: Text(context.localization!.logout),
-                      onTap: () => {
-
-                      },
-                    )
-                  ]
-              ).toList())
-          )
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (_) => ThemeModel(),
+          ),
         ],
-      ),
+      child: Consumer<ThemeModel>(
+        builder: (_, ThemeModel themeNotifier, __){
+
+          if(themeNotifier.isDark != null){
+            isDarkTheme = themeNotifier.isDark!;
+          }
+
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(context.localization!.settings),
+            ),
+            body: ListView(
+              children: [
+                Padding(padding: const EdgeInsets.symmetric(horizontal: paddingSmall), child:
+                Column(
+                    children:
+                    ListTile.divideTiles(context: context,
+                        tiles: [
+                          ListTile(
+                            leading: const Icon(Icons.brightness_medium),
+                            title: Text(context.localization!.darkMode),
+                            trailing: Switch(value: isDarkTheme, onChanged: (bool value){
+                              setState(() {
+                                themeNotifier.isDark = value;
+                              });
+                            },),
+                          ),
+                          ListTile(
+                            leading: const Icon(Icons.language),
+                            title: Text(context.localization!.language),
+                            trailing: const Icon(Icons.arrow_forward),
+                            onTap: () => {
+
+                            },
+                          ),
+                          ListTile(
+                            leading: const Icon(Icons.info_outline),
+                            title: Text(context.localization!.licenses),
+                            trailing: const Icon(Icons.arrow_forward),
+                            onTap: () => {
+
+                            },
+                          ),
+                          ListTile(
+                            leading: const Icon(Icons.logout),
+                            title: Text(context.localization!.logout),
+                            onTap: () => {
+
+                            },
+                          )
+                        ]
+                    ).toList())
+                )
+              ],
+            ),
+          );
+        },
+      )
     );
   }
 }
