97c6af7f148a995c87291e4f30f940780ccb0870
Felix Thape <felix.thape@gmail.com>
2023-03-10 22:57:34 +0100

    feat: add bottom appbar

--- apps/tasks/lib/components/bed_position_indicator.dart
+++ apps/tasks/lib/components/bed_position_indicator.dart
@@ -1,48 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:helpwave_localization/localization.dart';
-import 'package:helpwave_widget/loading.dart';
+import 'package:helpwave_theme/constants.dart';
 
 /// An Indicator to show at which bed position in a [Room] the [Patient] is
 ///
-/// 1 | 2 | 3 | 4 is an example for the bed counting order
+/// 0 | 1 | 2 | 3 | 4 is an example for the bed counting order
 class BedPositionIndicator extends StatelessWidget {
   /// The number of beds in the room
   final int totalBedCount;
 
   /// The position of bed in the room
   ///
-  /// please note first bed is position 1
+  /// please note first bed is position 0
   final int bedPosition;
+
+  /// The [Size] of the bed indicators
+  final Size bedIndicatorSize;
+
+  /// Whether the explanatory [Text] below the indicators is shown
+  final bool isHidingText;
+
+  /// The [Color] of the current [Bed]
+  final Color activeColor;
+
+  /// The [Color] of the not indicated [Bed]
+  final Color inactiveColor;
+
+  /// The radius of indicators
+  final double borderRadius;
+
+  /// The radius of indicators
+  final double distanceBetween;
 
   const BedPositionIndicator({
     super.key,
     this.totalBedCount = 4,
-    this.bedPosition = 2,
-  });
+    this.bedPosition = 1,
+    this.bedIndicatorSize = const Size(12, 20),
+    this.isHidingText = false,
+    this.distanceBetween = 6,
 
-  // TODO replace API-Call
-  Future<List<Map<String, dynamic>>> loadTasks() async {
-    return [
-      {"name": "Taskname"},
-    ];
-  }
+    /// TODO change colors with ThemeUpdate
+    this.activeColor = Colors.purple,
+    this.inactiveColor = Colors.grey,
+    this.borderRadius = 3,
+  }) : assert(bedPosition < totalBedCount);
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: loadTasks(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Scaffold(
-            body: Center(
-              child: LoadErrorWidget(
-                errorText: context.localization!.errorOnLoad,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            for (int i = 0; i <= totalBedCount; i++)
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: distanceBetween / 2,
+                ),
+                child: Container(
+                  height: bedIndicatorSize.height,
+                  width: bedIndicatorSize.width,
+                  decoration: BoxDecoration(
+                    color: i == bedPosition ? activeColor : inactiveColor,
+                    borderRadius: BorderRadius.circular(borderRadius),
+                  ),
+                ),
+              )
+          ],
+        ),
+        isHidingText
+            ? const SizedBox()
+            : Padding(
+                padding: const EdgeInsets.only(top: distanceTiny),
+                child: Text(
+                  context.localization!.bedPosition,
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
               ),
-            ),
-          );
-        }
-        return Scaffold();
-      },
+      ],
     );
   }
 }
--- apps/tasks/lib/screens/patient_screen.dart
+++ apps/tasks/lib/screens/patient_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:helpwave_localization/localization.dart';
+import 'package:helpwave_theme/constants.dart';
+import 'package:helpwave_widget/loading.dart';
+import 'package:tasks/components/bed_position_indicator.dart';
 
 class PatientScreen extends StatelessWidget {
   final Map<String, dynamic> patient;
@@ -8,8 +12,90 @@ class PatientScreen extends StatelessWidget {
     required this.patient,
   });
 
+  // TODO replace API-Call
+  Future<List<Map<String, dynamic>>> loadTasks() async {
+    return [
+      {"name": "Taskname", "description": "This is a very desriptive text"},
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    const double bottomBarHeight = 60;
+
+    return Scaffold(
+      appBar: AppBar(title: Text(patient["name"])),
+      bottomNavigationBar: SizedBox(
+        height: bottomBarHeight,
+        child: BottomAppBar(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(paddingSmall),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: distanceTiny),
+                    child: Text(
+                      patient["room"]["name"],
+                      style: Theme.of(context).textTheme.titleLarge,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
+                const Expanded(
+                    child: BedPositionIndicator(
+                  isHidingText: true,
+                )),
+                // TODO change Color with Theme update
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton.icon(
+                        style: ButtonStyle(
+                          shape: MaterialStatePropertyAll(
+                            RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(borderRadiusBig),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {
+                          // TODO open TaskCreationScreen with patient and room information
+                        },
+                        icon: const Icon(Icons.add, size: iconSizeTiny),
+                        label: Text(context.localization!.addTasks),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: FutureBuilder(
+        future: loadTasks(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: LoadErrorWidget(
+                errorText: context.localization!.errorOnLoad,
+              ),
+            );
+          }
+          if (!snapshot.hasData) {
+            Center(
+              child: LoadingSpinner(
+                text: "${context.localization!.loading}...",
+              ),
+            );
+          }
+          return ListView();
+        },
+      ),
+    );
   }
 }
