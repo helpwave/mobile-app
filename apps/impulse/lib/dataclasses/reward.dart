763727275c4ab40eb1c4587fb3d71dd45a8c7a0d
Florian Jafar Sabonchi <sabonchi@posteo.de>
2023-09-23 00:03:08 +0200

    feat: add reward class

---
+++ apps/impulse/lib/dataclasses/reward.dart
@@ -0,0 +1,13 @@
+class Reward{
+  String id;
+  String title;
+  String description;
+  int points;
+
+  Reward({
+    required this.title,
+    required this.description,
+    required this.points,
+    required this.id,
+  });
+}
