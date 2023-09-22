71175d08e27000dbde2e63855d65ed6fe4509c8f
Felix Thape <felix.thape@gmail.com>
2023-09-23 00:25:17 +0200

    feat: add home screen

--- apps/impulse/lib/main.dart
+++ apps/impulse/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:impulse/components/medal_carusel.dart';
+import 'package:helpwave_theme/constants.dart';
+import 'package:impulse/screens/HomeScreen.dart';
 import 'package:impulse/theming/colors.dart';
-import 'package:impulse/components/progressbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,69 +16,31 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'helpwave impulse',
       theme: ThemeData(
+        primaryColor: primary,
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          surfaceTintColor: Colors.transparent,
+        ),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        chipTheme: const ChipThemeData(
+          side: BorderSide.none,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(40),
+            ),
+          ),
+        ),
+        cardTheme: const CardTheme(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(borderRadiusBig),
+            ),
+          ),
+        ),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Color(0xFFA49AEC), primary],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          stops: [0.0, 1.0],
-        ),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          title: Text(
-            widget.title,
-            style: const TextStyle(color: Colors.white),
-          ),
-        ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ProgressBar(progress: 0.5),
-              MedalCarousel(),
-            ],
-          ),
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
