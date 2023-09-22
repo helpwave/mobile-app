c9a630f66a77be1f0e098b9904680ab995dae021
LauraLobster <braaklaura@gmail.com>
2023-09-22 19:35:33 +0200

    fiat: further work on structure

--- apps/impulse/lib/components/challange_menu_card.dart
+++ apps/impulse/lib/components/challange_menu_card.dart
@@ -2,11 +2,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ChallengeMenuCard extends StatelessWidget {
-  const ChallengeMenuCard({super.key, required this.title, required this.subtitle, required this.text});
+  const ChallengeMenuCard(
+      {super.key,
+      required this.title,
+      required this.subtitle,
+      required this.text});
 
   final String title;
   final String subtitle;
   final String text;
+
+  static const titleColor = Color(0xff6A5AE0);
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +22,38 @@ class ChallengeMenuCard extends StatelessWidget {
         padding: const EdgeInsets.all(20),
         child: Column(
           children: <Widget>[
-            Text(title, textAlign: TextAlign.center, style: TextStyle(color: Colors.purple),),
-            Text(subtitle, textAlign: TextAlign.center),
-            Text(text, textAlign: TextAlign.center,),
-            Text("ChallengePicture"),
+            Text(
+              title,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  color: titleColor, fontWeight: FontWeight.w700, fontSize: 30),
+            ),
+            Text(
+              subtitle,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  color: titleColor, fontWeight: FontWeight.w700, fontSize: 22),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                text,
+                textAlign: TextAlign.center,
+                style:
+                    const TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
+              ),
+            ),
+            Container(
+              color: titleColor,
+              width: 215,
+              height: 215,
+              padding: EdgeInsets.all(4),
+              child: ClipRRect(
+                child: Container(
+                  color: Colors.blue,
+                ),
+              ),
+            ),
             Text("NextButton")
           ],
         ),
