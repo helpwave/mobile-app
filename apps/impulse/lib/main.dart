ea4c55a3cd569783f08da746d4ef40e0eff49962
Felix Thape <felix.thape@gmail.com>
2023-09-22 23:04:33 +0200

    chore: merge main

--- apps/impulse/lib/main.dart
+++ apps/impulse/lib/main.dart
@@ -73,13 +73,13 @@ class _MyHomePageState extends State<MyHomePage> {
             style: const TextStyle(color: Colors.white),
           ),
         ),
-        body: Center(
+        body: const Center(
           // Center is a layout widget. It takes a single child and positions it
           // in the middle of the parent.
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const MedalCarousel(),
+              MedalCarousel(),
             ],
           ),
         ),
