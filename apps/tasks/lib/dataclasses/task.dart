14443094df24897ebe3837cc8a8e168caf1bd8e7
Felix Thape <felix.thape@gmail.com>
2023-09-27 16:56:05 +0200

    feat: add TaskCards and aggregate on my tasks screen

--- apps/tasks/lib/dataclasses/task.dart
+++ apps/tasks/lib/dataclasses/task.dart
@@ -1,4 +1,5 @@
 // TODO delete later and import from protobufs
+import 'package:tasks/dataclasses/patient.dart';
 import 'package:tasks/dataclasses/subtask.dart';
 
 enum TaskStatus {
@@ -20,6 +21,20 @@ class Task {
   DateTime? creationDate;
   bool isPublicVisible;
 
+  double get progress =>  subtasks.isNotEmpty ? subtasks.where((element) => element.isDone).length / subtasks.length
+      : 1;
+
+  /// the remaining time until a task is due
+  ///
+  /// **NOTE**: returns [Duration.zero] if [dueDate] is null
+  Duration get remainingTime => dueDate != null ? dueDate!.difference(DateTime.now()) : Duration.zero;
+
+  bool get isOverdue => remainingTime.isNegative;
+
+  bool get inNextTwoDays => remainingTime.inDays < 2;
+
+  bool get inNextHour => remainingTime.inHours < 1;
+
   Task({
     required this.id,
     required this.name,
@@ -30,5 +45,22 @@ class Task {
     this.dueDate,
     this.creationDate,
     this.isPublicVisible = false,
+  });
+}
+
+class TaskWithPatient extends Task {
+  PatientMinimal patient;
+
+  TaskWithPatient({
+    required super.id,
+    required super.name,
+    required super.assignee,
+    required super.notes,
+    super.status,
+    super.subtasks,
+    super.dueDate,
+    super.creationDate,
+    super.isPublicVisible,
+    required this.patient,
   });
 }
