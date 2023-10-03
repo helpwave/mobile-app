4e8e0ae2ee36929d875bb9093969df5e673bda52
Felix <67233923+DasProffi@users.noreply.github.com>
2023-10-03 23:43:33 +0200

    feat: add sparkle (#363)
    * feat: add sparkle
    
    * refactor: use inbuilt pi constant

--- apps/impulse/lib/components/medal_carousel.dart
+++ apps/impulse/lib/components/medal_carousel.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:impulse/animations/sparkle_animation.dart';
 import 'package:impulse/components/medal.dart';
 import 'package:impulse/util/level.dart';
 
@@ -21,7 +22,26 @@ class _MedalCarouselState extends State<MedalCarousel> {
       items: List.generate(maxLvl, (index) => index + 1).map((i) {
         return Builder(
           builder: (BuildContext context) {
-            return Medal(name: "Level $i", icon: widget.unlockedTo >= i ? Icons.workspace_premium_rounded : Icons.lock,);
+            return Stack(
+              children: [
+                Center(
+                  child: Medal(
+                    name: "Level $i",
+                    icon: widget.unlockedTo >= i ? Icons.workspace_premium_rounded : Icons.lock,
+                  ),
+                ),
+                widget.unlockedTo >= i
+                    ? Positioned(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SparkleAnimation(width: MediaQuery.of(context).size.width * 0.5, height: 100),
+                          ],
+                        ),
+                      )
+                    : Container(),
+              ],
+            );
           },
         );
       }).toList(),
