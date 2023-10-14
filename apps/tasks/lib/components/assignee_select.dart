0ff00f6c5b5d50073a2f732558914a89d26aa33e
Felix Thape <felix.thape@gmail.com>
2023-10-14 14:15:02 +0200

    feat: add assignee component base

---
+++ apps/tasks/lib/components/assignee_select.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AssigneeSelect extends StatelessWidget {
+  const AssigneeSelect({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
