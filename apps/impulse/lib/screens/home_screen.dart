8892cc3b52f2df23a922aa2908c1e537b2c3625d
Florian <54689374+florian-sabonchi@users.noreply.github.com>
2023-10-21 14:57:59 +0200

    feat: Update apps/impulse/lib/screens/home_screen.dart
    Co-authored-by: Felix <67233923+DasProffi@users.noreply.github.com>

--- apps/impulse/lib/screens/home_screen.dart
+++ apps/impulse/lib/screens/home_screen.dart
@@ -68,7 +68,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => ProfileScreen(
-                    initialUser: user.id.isNotEmpty ? user : null,
+                    initialUser: user,
                   )),
                 );
               },
