9e3795319b8e344ef887dc98798a4c9bd4274eb2
Felix Thape <felix.thape@gmail.com>
2023-11-07 13:10:07 +0100

    chore: wrap unsafe getUserInfo in a try catch

--- packages/helpwave_service/lib/src/auth/authentication_service.dart
+++ packages/helpwave_service/lib/src/auth/authentication_service.dart
@@ -106,7 +106,12 @@ class AuthenticationService {
     if (credential == null) {
       return null;
     }
-    UserInfo userInfo = await credential.getUserInfo();
+    UserInfo userInfo;
+    try {
+      userInfo = await credential.getUserInfo();
+    } catch (e) {
+      return null;
+    }
     return _toIdentity(credential, userInfo);
   }
 
