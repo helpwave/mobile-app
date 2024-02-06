561a645ef0d488df5b5f5a9e6896adf3d7c91311
Felix Thape <felix.thape@gmail.com>
2024-02-06 17:33:09 +0100

    fix: fix linting

--- apps/tasks/lib/services/current_ward_svc.dart
+++ apps/tasks/lib/services/current_ward_svc.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tasks/config/config.dart';
 import 'package:tasks/dataclasses/organization.dart';
